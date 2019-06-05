b461559e72c349d67c261c627edf5dd6b85a93f5
Tristan Reed <mail@trisreed.com>
2019-06-05 10:20:38 +0800

    Remove packages that have been removed from solution.

--- osmconverter.py
+++ osmconverter.py
@@ -14,15 +14,11 @@ __version__ = "0.1.0"
 
 
 """ Import required libraries. """
-import functools, json, pyproj, sys, textwrap, tqdm
-import shapely.geometry, shapely.ops, shapely.speedups # macOS is weird.
+import pyproj, sys, textwrap, tqdm
 import xml.etree.cElementTree as ElementTree
 
 
 def main():
-
-    """ Enable speedups. """
-    shapely.speedups.enable()
 
     """ Check that we have supplied four arguments. """
     if (len(sys.argv) == 5):
